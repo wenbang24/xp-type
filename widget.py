a47d64e827cd1444db4269803ee9e99461413f8b
wenbang24 <wenbangalt@gmail.com>
2025-07-19 18:10:00 +1000

    add coloring and keypress capturing

--- widget.py
+++ widget.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
 
 from MainWindow import Ui_XPType
 
@@ -8,10 +8,34 @@ class XPType(QtWidgets.QMainWindow, Ui_XPType):
     def __init__(self, *args, obj=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
+        self.originalText = self.label.text()
+
+    def update_label(self, text):
+        self.label.setText(text)
+        self.originalText = text
+
+    def color(self, n):
+        styled_text = (
+            f'<span style="color: white;">{self.originalText[:n]}</span>'
+            f'<span style="color: grey;">{self.originalText[n:]}</span>'
+        )
+        self.label.setText(styled_text)
+
+    def keyPressEvent(self, a0):
+        key = a0.key()
+        text = a0.text()
+
+        print(f"Key Pressed: {key}, Text: '{text}'")
+
+        super().keyPressEvent(a0)
 
 
 app = QtWidgets.QApplication(sys.argv)
 
 window = XPType()
 window.show()
+
+for i in range(20):
+    QtCore.QTimer.singleShot(i * 1000, lambda: window.color(i))
+
 app.exec()
